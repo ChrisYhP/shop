540e04ccdf7359ea0e6d82eb974392a27a92b7f2
chris <616563259@qq.com>
2019-10-29 22:20:52 +0800

    添加配置

---
+++ migo-manage/manage-web/src/main/java/com/migo/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.migo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PageController {
+  @RequestMapping(value = "/page/{pagename}", method = RequestMethod.GET)
+  public String tpage(@PathVariable String pageName) {
+     return pageName;                                                                        
+  }
+}
